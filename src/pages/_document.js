5c1688d82568c60f2baeefc6e7447e75239ab941
MKLSV <mkolosov96@gmail.com>
2024-12-14 11:55:33 +0300

    apple

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,13 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
- 
+
   return (
     <Html lang="en">
       <Head />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       <body>
         <Main />
         <NextScript />
