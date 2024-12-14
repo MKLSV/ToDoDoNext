e6a40ccaa85e25af1fd816c118763cbb1e04a691
MKLSV <mkolosov96@gmail.com>
2024-12-14 11:12:15 +0300

    asdad

---
+++ src/components/Loader copy.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Loader() {
+
+    return (
+        <div className="loader">
+            <div className="wrapper">
+                <div className="circle" />
+                <div className="circle" />
+                <div className="circle" />
+                <div className="shadow" />
+                <div className="shadow" />
+                <div className="shadow" />
+            </div>
+        </div>
+    );
+}
+
+
