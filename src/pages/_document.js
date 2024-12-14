8c6d54340914cde47c0187f24b9f8d1cd7cc2312
MKLSV <mkolosov96@gmail.com>
2024-12-14 12:05:17 +0300

    dd

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple.png" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       <body>
